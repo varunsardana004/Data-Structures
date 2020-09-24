dde72f9601a3c08fd7bcc33c1a27ec97ab11a52f
Varun Sardana <54537819+varunsardana004@users.noreply.github.com>
2020-09-25 00:12:07 +0530

    Update reversing_doubly_linked_list.java

--- reversing_doubly_linked_list.java
+++ reversing_doubly_linked_list.java
@@ -1,34 +1,39 @@
 import java.util.*;
-public class Main{
-    
-  class Node
+public class Main
 {
-    int data;
-    Node next;
-     Node previous;  
-     
-    Node(int data)
-    {
-        this.data=data;
-        next=null;
-    }
-}
+    Node head=null;
+    Node tail=null;
+    static class Node{
+        int data;
+        Node next;
+        Node previous;
     
-  Node head,tail=null;
-  public void insertNode(int data) {  
-        Node newNode = new Node(data);  
-        if(head == null) {  
-             head = tail = newNode;  
-            head.previous = null;  
-            tail.next = null;  
-        }  
-        else {  
-            tail.next = newNode;  
-             newNode.previous = tail;  
-            tail = newNode;  
-            tail.next = null;  
-        }  
-    }  
+    Node(int item){
+        data=item;
+        next=null;
+        previous=null;
+    }
+    }
+    public void insert(int data)
+    {
+        Node newnode=new Node(data);
+        Node temp=head;
+        if(head==null)
+        {
+            head=temp=newnode;
+        }
+        else
+        {
+            while(temp.next!=null)
+            {
+                temp=temp.next;
+            }
+            temp.next=newnode;
+            newnode.previous=temp;
+            temp=newnode;
+            tail=temp;
+        }
+    }
     public void reverse()
     {
         Node currentnode=head;
@@ -49,28 +54,33 @@ public class Main{
         tail=currentnode;
         }
     }
-    public  void show()
-    {
-        Node temp=head;
-        while(temp!=null)
+   public void display(){
+        if(head==null)
         {
-            System.out.println(temp.data);
-            temp=temp.next;
+            return ;
         }
-    }
-     public static void main(String []args){
-      Main ob=new Main();
-      Scanner s1=new Scanner(System.in);
-      ob.insertNode(10); 
-      ob.insertNode(20);
-      ob.insertNode(30); 
-      ob.insertNode(40);
-      ob.insertNode(50);
-     System.out.println("the initial Linked List is");
-      ob.show();
-      ob.reverse();
-       System.out.println("the linked list after reversing : ");
-       ob.show();
-       
-    
-     }}
+        Node current=head;
+        while(current!=null){
+            System.out.print(current.data+ " ");
+            current=current.next;
+        }
+   }
+     public static void main(String[] args) {
+	
+		Main ob=new Main();
+      Scanner s=new Scanner(System.in);
+      
+      System.out.println("Enter No of elements to insert in doubly linked list.");
+	    int n=s.nextInt();
+	    System.out.println("Enter list elements.");
+	    for(int i=0;i<n;i++){
+	        ob.insert(s.nextInt());
+	    }
+	     System.out.println("Doubly Linked list : ");
+	  ob.display();
+	    ob.reverse();
+	    System.out.println("\nDoubly Linked list after reversing : ");
+	  ob.display();
+	    
+	}
+}
