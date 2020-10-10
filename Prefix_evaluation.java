a4f60cea1ad458ecbbc4c52efd3d7af180aad5f7
Varun Sardana <54537819+varunsardana004@users.noreply.github.com>
2020-10-10 21:04:01 +0530

    Update Prefix_evaluation.java

--- Prefix_evaluation.java
+++ Prefix_evaluation.java
@@ -5,12 +5,12 @@ class Main
     int i, op1, op2, answer;
     for (i = exp.length()-1; i >= 0 ; i--)
     {
-      // If the scanned character is an operand (number here), push it to the stack.
+      
       char c = exp.charAt(i);
       if (Character.isDigit(c))
         s.push(c - '0');
 
-      // If the scanned character is an operator, pop two elements from stack apply the operator
+    
       else
       {
         op1 = s.pop();
